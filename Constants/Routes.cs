20734d3371385cb44e409a1c6caac34fe6900a82
mrzaxaryan <mrzaxaryan>
2026-02-04 13:22:14 -0800

    feat: Implement service worker for caching and update notifications, add update notification styles, and enhance manifest file

--- Constants/Routes.cs
+++ Constants/Routes.cs
@@ -4,9 +4,6 @@ public static class Routes
 {
     public const string Home = "/";
     public const string Blog = "#open-source";
-    public const string BlogEc = "/open-source/ec";
-    public const string BlogCppPic = "/open-source/cpp-pic";
-    public const string BlogNoRwx = "/open-source/norwx";
     public const string Careers = "#careers";
     public const string Privacy = "#privacy";
     public const string Services = "#services";
