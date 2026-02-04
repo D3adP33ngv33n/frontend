8c3fa890a5cd7c6c6177aa7e4010bc3b64494841
mrzaxaryan <mrzaxaryan>
2026-02-04 03:12:03 -0800

    feat: Refactor routing and enhance Blog section; update Navbar links and Privacy component styling

--- Constants/Routes.cs
+++ Constants/Routes.cs
@@ -3,13 +3,15 @@ namespace Nostdlib.Constants;
 public static class Routes
 {
     public const string Home = "/";
-    public const string Blog = "/blog";
+    public const string Blog = "#blog";
     public const string BlogEc = "/blog/ec";
     public const string BlogCppPic = "/blog/cpp-pic";
     public const string BlogNoRwx = "/blog/norwx";
-    public const string Careers = "/#careers";
-    public const string Privacy = "/privacy";
-    public const string Security = "/security";
+    public const string Careers = "#careers";
+    public const string Privacy = "#privacy";
+    public const string Services = "#services";
+    public const string About = "#about";
+    public const string Contact = "#contact";
 
     public static string GetCareerDetailsUrl(int id) => $"/careers/{id}";
 }
