b4e29059b8eb87e59cd72dd3c6f5056716f2e841
mrzaxaryan <mrzaxaryan>
2026-03-17 11:55:29 +0400

    Remove LnkTool and PeParser pages along with their associated styles and navigation links; update locale files to reflect these changes.

--- Constants/Routes.cs
+++ Constants/Routes.cs
@@ -3,9 +3,6 @@ namespace Nostdlib.Constants;
 public static class Routes
 {
     public const string Home = "/";
-public const string Careers = "#careers";
+    public const string Careers = "#careers";
     public const string Contact = "#contact";
-    public const string LnkTool = "/lnk-tool";
-    public const string Base64Converter = "/base64-converter";
-    public const string PeParser = "/pe-parser";
 }
